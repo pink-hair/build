1ef342afae5d4af9f294338c6b9cd8b87462dd58
Zak <zaksir@polytech.blog>
2018-05-03 11:00:29 -0700

    TOKENIZE

--- TokenizeFile.ps1
+++ TokenizeFile.ps1
@@ -10,6 +10,11 @@ $builder = new-object System.Text.StringBuilder;
 
 foreach($line in $content)
 {
+    if($line.IndexOf($token) != -1)
+    {
+        Write-Warning("Token Found");
+    }
+
     $newLine = $line.Replace($token, $replacementValue);
     [void]$builder.AppendLine($line);
 }
